c931593d3b8210398307becefd19864a30f3a848
hendriksandy <79459594+hendriksandy@users.noreply.github.com>
2021-05-28 03:12:47 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const buttons = document.querySelectorAll(".big-five-button")
+buttons.forEach((button) => {
+    button.addEventListener("click", function () {
+        const bigFiveList = document.createElement("li");
+        const spottedAnimals = document.getElementById("spotted-animals-list");
+        spottedAnimals.appendChild(bigFiveList);
+        bigFiveList.innerHTML = event.target.innerHTML;
+    });
+});
+
+const removeFirstAnimaleButton = document.getElementById("remove-first-item-button")
+
+removeFirstAnimaleButton.addEventListener("click", function () {
+    const Parent = document.getElementById("spotted-animals-list")
+    const TopAnimalOnTheList = Parent.getElementsByTagName("li")[0]
+    Parent.removeChild(TopAnimalOnTheList)
+});
+
+const removeAllAnimals = document.getElementById("remove-all-button")
+
+removeAllAnimals.addEventListener("click", function () {
+    const parent = document.getElementById("spotted-animals-list")
+    const topAnimalOnTheList = parent.getElementsByTagName("li")
+    parent.remove(topAnimalOnTheList)
+})
